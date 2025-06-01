4b029b53c0609c65cf8c51f41eaa3e4ec7f08ecd
Valerio Sacco <valeriosacco@outlook.it>
2025-06-01 15:36:10 +0200

    [product service]: removed warnings

--- src/PhotoSi.ProductsService/Models/Product.cs
+++ src/PhotoSi.ProductsService/Models/Product.cs
@@ -5,11 +5,11 @@ namespace PhotoSi.ProductsService.Models
     public class Product : BaseModel
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
         public Guid CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category Category { get; set; } = null!;
     }
 }
